b872342cf05c0a47300bb7777aed54fbd4925fea
Daniel <dhaavi@users.noreply.github.com>
2018-11-15 22:44:51 +0100

    Add host network to container

--- start_container.sh
+++ start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" && pwd )"
-docker run -ti --rm --name go-env -v "$SCRIPT_DIR/.cache:/home/user" -v "$(pwd):$(pwd)" go-env
+docker run -ti --rm --name go-env -v "$SCRIPT_DIR/.cache:/home/user" -v "$(pwd):$(pwd)" --network=host go-env
